65924be26aa5b6c8357704c40613239a5e129d6f
dvhar <dh22@protonmail.com>
2020-10-30 21:27:56 -0500

    update build dep, remove test

--- csvquery.rb
+++ csvquery.rb
@@ -8,11 +8,11 @@ class Csvquery < Formula
   sha256 "e516ed22278a511aa8cbfd5b095f48756ac4e66ca1fa6b1a9ead4951411f5636"
   license ""
 
-  depends_on "gcc"
+  depends_on "gcc" => :build
+  depends_on "cmake" => :build
   depends_on "tbb"
   depends_on "nlohmann-json"
   depends_on "boost"
-  depends_on "cmake" => :build
 
   def install
     gcc = Dir.glob('/usr/local/bin/gcc*').select{ |f| f =~ /gcc(-[0-9]+)?$/ }[0]
@@ -23,16 +23,4 @@ class Csvquery < Formula
     cp_r "cql", prefix
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test csvquery`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
