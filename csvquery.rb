fb81be622bd22a65de7b5e55ee9450b9780f2342
d <d@ds-iMac.local>
2020-10-30 20:27:32 -0500

    update formula

--- csvquery.rb
+++ csvquery.rb
@@ -1,6 +1,6 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+require 'etc'
 class Csvquery < Formula
   desc "SQL interpreter for big csv files"
   homepage ""
@@ -8,16 +8,19 @@ class Csvquery < Formula
   sha256 "e516ed22278a511aa8cbfd5b095f48756ac4e66ca1fa6b1a9ead4951411f5636"
   license ""
 
-  # depends_on "cmake" => :build
+  depends_on "gcc"
+  depends_on "tbb"
+  depends_on "nlohmann-json"
+  depends_on "boost"
+  depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+    gcc = Dir.glob('/usr/local/bin/gcc*').select{ |f| f =~ /gcc(-[0-9]+)?$/ }[0]
+    gpp = Dir.glob('/usr/local/bin/g++*').select{ |f| f =~ /g\+\+(-[0-9]+)?$/ }[0]
+    ncores = Etc.nprocessors
+    system { "CC" => gcc, "CXX" => gpp }, "cmake", "."
+    system "make", "-j"+ncores.to_s
+    cp_r "cql", prefix
   end
 
   test do
