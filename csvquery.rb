82bfe30ee803a07afe798e664ada7d169d77d3d2
dvhar <dh22@protonmail.com>
2020-11-11 21:39:33 -0600

    1.13

--- csvquery.rb
+++ csvquery.rb
@@ -3,8 +3,8 @@
 class Csvquery < Formula
   desc "SQL interpreter for big csv files"
   homepage "https://davosaur.com/csv"
-  url "https://github.com/dvhar/csvquery/archive/1.12.tar.gz"
-  sha256 "771012912df9eec8cd8264efe27eba8aaf24175193b8afa92ca7c64406fb2094"
+  url "https://github.com/dvhar/csvquery/archive/1.13.tar.gz"
+  sha256 "d3f6efe99fb200c44e88b7c11fe302ed8323dc9e19fa86958e349a2915583d3d"
   license ""
 
   depends_on "cmake" => :build
