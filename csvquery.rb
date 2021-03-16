6ff1d5f3fc494ba7c3422466344aad15d41f500b
dvhar <dh22@protonmail.com>
2021-03-15 21:13:36 -0500

    1.28

--- csvquery.rb
+++ csvquery.rb
@@ -1,10 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class Csvquery < Formula
   desc "SQL-like interpreter for big csv files"
-  homepage "https://davosaur.com/csv"
-  url "https://github.com/dvhar/csvquery/archive/1.27.tar.gz"
-  sha256 "d17b526d49e9128cbc5dd73fadc455a172b384df20c343977e5e94b956a72aac"
+  homepage "https://github.com/dvhar/csvquery"
+  url "https://github.com/dvhar/csvquery/archive/1.28.tar.gz"
+  sha256 "ebe6604f139833d9b8eb404ffc4118976584b5f3b215967c22a0369224e93f39"
   license "GLP-3"
 
   depends_on "cmake" => :build
